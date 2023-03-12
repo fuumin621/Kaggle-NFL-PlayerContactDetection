c912a95e11bd8129361588e9778112899473d27a
sqrt4kaido <44626741+root4kaido@users.noreply.github.com>
2023-03-12 17:12:28 +0900

    Update train.sh

--- code/train.sh
+++ code/train.sh
@@ -4,10 +4,11 @@ python train_1st.py --config_path ./../config/NFL_resnext50-TSM_LSTM_16-16_cp-3w
 python train_1st.py --config_path ./../config/NFL_resnext50-TSM_LSTM_32-32_cp-3wh_few_128_drop04_flow_aug.yaml
 python train_1st.py --config_path ./../config/NFL_resnext50-TSM_LSTM_64-64_cp-3wh_few_128_drop04_flow_aug.yaml
 
-
 python train_2nd.py --config_path ./../config/NFL_effnet-b0-TSM-end_LSTM_32-32_cp-3wh_few_128_drop04_flow_aug_2nd_lgbm.yaml
 python train_2nd.py --config_path ./../config/NFL_effnet-b0-TSM-end_LSTM_64-64_cp-3wh_few_128_drop06_flow_aug_2nd_lgbm.yaml
 python train_2nd.py --config_path ./../config/NFL_resnext50-TSM_LSTM_16-16_cp-3wh_few_128_drop04_flow_aug_2nd_xgboost.yaml
 python train_2nd.py --config_path ./../config/NFL_resnext50-TSM_LSTM_32-32_cp-3wh_few_128_drop04_flow_aug_2nd_lgbm.yaml
 python train_2nd.py --config_path ./../config/NFL_resnext50-TSM_LSTM_64-64_cp-3wh_few_128_drop04_flow_aug_2nd_lgbm.yaml
 python train_2nd.py --config_path ./../config/NFL_resnext50-TSM_LSTM_64-64_cp-3wh_few_128_drop04_flow_aug_2nd_xgboost.yaml
+
+python postprocess.py --config_path ./../config/NFL_postprocess.yaml
